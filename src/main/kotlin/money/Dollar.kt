3b0b8add202a6ec03a29e597625e279ebf39fa46
kei711 <kei.yam.711@gmail.com>
2020-12-29 10:50:39 +0900

    1-3

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -2,4 +2,9 @@ package money
 
 class Dollar(var amount: Int) {
     fun times(multiplier: Int): Dollar = Dollar(amount * multiplier)
+
+    override fun equals(other: Any?): Boolean {
+        val dollar = other as Dollar
+        return amount == dollar.amount
+    }
 }
--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -2,6 +2,8 @@ package money
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class MoneyTest {
     @Test
@@ -11,5 +13,11 @@ class MoneyTest {
         assertEquals(10, product.amount)
         product = five.times(3)
         assertEquals(15, product.amount)
+    }
+
+    @Test
+    fun testEquality() {
+        assertTrue(Dollar(5).equals(Dollar(5)))
+        assertFalse(Dollar(5).equals(Dollar(6)))
     }
 }
