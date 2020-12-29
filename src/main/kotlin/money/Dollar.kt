ff16901cde915ee8d6f55026660c07fae11a184c
kei711 <kei.yam.711@gmail.com>
2020-12-29 10:53:05 +0900

    1-4

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,6 +1,6 @@
 package money
 
-class Dollar(var amount: Int) {
+class Dollar(private val amount: Int) {
     fun times(multiplier: Int): Dollar = Dollar(amount * multiplier)
 
     override fun equals(other: Any?): Boolean {
