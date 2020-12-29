919549d68737970d49b3950ee9a2c8b52abd81ad
kei711 <kei.yam.711@gmail.com>
2020-12-29 10:50:01 +0900

    1-2

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,8 +1,5 @@
 package money
 
 class Dollar(var amount: Int) {
-    fun times(multiplier: Int) {
-        amount *= multiplier
-    }
-
+    fun times(multiplier: Int): Dollar = Dollar(amount * multiplier)
 }
